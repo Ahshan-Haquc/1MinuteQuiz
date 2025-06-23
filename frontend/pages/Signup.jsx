f9da8bbc3ae241cfc2a9adf22aec19d78374b1f5
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2025-06-23 13:10:43 +0600

    trying to apply tailwindcss

---
+++ frontend/pages/Signup.jsx
@@ -0,0 +1,11 @@
+const Signup = () => {
+  return (
+    <div className="h-full w-full center">
+      <div className="h-[400px] md:h-[300px] w-[200px] md:w-[300px] p-2 md:p-4 bg-black">
+        <h1 className="text-center text-2xl font-bold mb-4">Sign Up</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import "./index.css";
-import viteLogo from "/vite.svg";
+import Signup from "../pages/Signup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <div className="text-green-600 text-3xl">Hello Ahsan</div>
-        <h1 className="text-3xl font-bold text-red-600">Hello Tailwind!</h1>
-
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-screen bg-[#EBF4F6]">
+        <Signup />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
